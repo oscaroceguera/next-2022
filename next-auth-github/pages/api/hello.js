2266674fe7570accc991e71b4786ceb5e3cffe31
Oscar E. Oceguera Bibriesca <oscar.oceguera@payclip.com>
2023-03-05 00:49:39 -0700

    some next-auth

---
+++ next-auth-github/pages/api/hello.js
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { unstable_getServerSession } from "next-auth/next";
+import { authOptions } from "../api/auth/[...nextauth]";
+
+export default async function handler(req, res) {
+  const session = await unstable_getServerSession(req, res, authOptions);
+  if (session) {
+    const blogData = [
+      {
+        id: 1,
+        title: "Blog 1",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
+        image: "/images/blog1.jpg",
+      },
+      {
+        id: 2,
+        title: "Blog 2",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.",
+        image: "/images/blog2.jpg",
+      },
+    ];
+    res.status(200).json(blogData);
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+}
